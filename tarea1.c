767ec5f365a0827134a50f43069b92c43c79528a
Your Name <you@example.com>
2025-04-08 03:21:12 -0400

    Luego de infinitos errores se corrigio la estrucutra Node. Se implemento para poder mostrar los tickets pendientes

--- tarea1.c
+++ tarea1.c
@@ -51,6 +51,13 @@ void mostrar_lista_tickets(List *tickets) {
     printf("No hay tickets pendientes. \n") ;
   } else {
     printf("Tickets pendientes: \n") ;
+
+    Node *nodo = tickets->head;
+    while (nodo != NULL) { 
+      Ticket *temp = (Ticket *)nodo->data;
+      printf("ID: %s, Prioridad: %d, Hora: %s", temp->id, temp->prioridad, ctime(&temp->hora));
+      nodo = nodo->next;
+    }
   }  
 }
 
