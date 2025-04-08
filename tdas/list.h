69bb2057c063f0489f9ebdc2fb12825721ed09d1
Your Name <you@example.com>
2025-04-08 03:03:49 -0400

    Pruebas

--- tdas/list.h
+++ tdas/list.h
@@ -1,43 +1,35 @@
 #ifndef LIST_H
 #define LIST_H
 
-typedef struct List List;
+#include <stdio.h>
+#include <stdlib.h>
 
-// Esta función crea una lista vacía y devuelve un puntero a la lista.
-List *list_create();
+// Definir la estructura Node
+typedef struct Node {
+    void *data;
+    struct Node *next;
+} Node;
 
-// Esta función devuelve un puntero al primer elemento de la lista.
-void *list_first(List *L);
+// Definir la estructura List
+typedef struct List {
+    Node *head;
+    Node *tail;
+    Node *current;
+    int size;
+} List;
 
-// Esta función mueve el puntero de la lista al siguiente elemento y devuelve un
-// puntero a dicho elemento.
-void *list_next(List *L);
-
-// Esta función inserta un nuevo elemento al inicio de la lista.
-void list_pushFront(List *L, void *dato);
-
-// Esta función inserta un nuevo elemento al final de la lista.
-void list_pushBack(List *L, void *dato);
-
-// Esta función inserta un nuevo elemento a continuación del actual de la lista.
-void list_pushCurrent(List *L, void *dato);
-
-// Esta función elimina el primer elemento de la lista.
-void *list_popFront(List *L);
-
-// Esta función elimina el último elemento de la lista.
-void *list_popBack(List *L);
-
-// Esta función elimina el elemento actual de la lista.
-void *list_popCurrent(List *L);
-
-// Esta función elimina todos los elementos de la lista.
+// Funciones de la lista
+List* list_create();
+void* list_first(List *L);
+void* list_next(List *L);
+void list_pushFront(List *L, void *data);
+void list_pushBack(List *L, void *data);
+void list_pushCurrent(List *L, void *data);
+void list_sortedInsert(List *L, void *data, int (*lower_than)(void *data1, void *data2));
+void* list_popFront(List *L);
+void* list_popBack(List *L);
+void* list_popCurrent(List *L);
+int list_size(List *L);
 void list_clean(List *L);
 
-// Función para insertar ordenado de acuerdo a la función lower_than
-void list_sortedInsert(List *L, void *data,
-                       int (*lower_than)(void *data1, void *data2));
-
-int list_size(List* L);
-
-#endif
+#endif // LIST_H
